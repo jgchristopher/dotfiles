611386b406635e2b3ccbe04da43abe33705cf759
John Christopher <jgchristopher@gmail.com>
2021-11-17 06:40:55 -0600

    adding my WIP nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,202 @@
+----------------------------- HELPERS -------------------------------------------
+local cmd = vim.cmd
+local fn = vim.fn
+local g = vim.g
+local opt = vim.opt
+
+
+
+require('plugins')
+require('nightfox').load("duskfox")
+
+require('lualine').setup {
+  options = {
+    -- ... your lualine config
+    theme = "nightfox"
+  }
+}
+
+
+
+-------------------------------------- NVIM Tree -----------------------------------------
+
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  auto_close          = false,
+  open_on_tab         = false,
+  hijack_cursor       = false,
+  update_cwd          = false,
+  update_to_buf_dir   = {
+    enable = true,
+    auto_open = true,
+  },
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  filters = {
+    dotfiles = false,
+    custom = {}
+  },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  }
+}
+
+
+
+
+
+
+
+
+-- Example config in lua
+-- g.nord_contrast = true
+-- g.nord_borders = false
+-- g.nord_disable_background = false
+-- g.nord_italic = false
+
+-- Load the colorscheme
+-- require('nord').set()
+
+-- cmd 'colorscheme nord'
+
+----------------------------- OPTIONS -------------------------------------------
+
+vim.opt.number = true
+vim.opt.list = true
+vim.opt.termguicolors = true
+vim.opt.autoindent = true
+
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+
+---------------------- TreeSitter ---------------------------------------
+local treeSitterConfigs = require("nvim-treesitter.configs")
+
+treeSitterConfigs.setup {
+  ensure_installed = "maintained",
+  highlight = {
+    enable = true
+  }
+}
+
+
+
+--------------------- LSP and Other Stuff --------------------------------------
+
+local path_to_elixirls = fn.expand("~/.local/share/nvim/lsp_servers/elixir/elixir-ls/language_server.sh")
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- For `vsnip` user.
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  },
+  formatting = {
+    format = require("lspkind").cmp_format({
+      with_text = true,
+      menu = {
+        nvim_lsp = "[LSP]",
+      },
+    }),
+  },
+})
+
+local on_attach = function(_, bufnr)
+  local function map(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  local map_opts = {noremap = true, silent = true}
+
+  map("n", "df", "<cmd>lua vim.lsp.buf.formatting()<cr>", map_opts)
+  map("n", "gd", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>", map_opts)
+  map("n", "dt", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
+  map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", map_opts)
+  map("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<cr>", map_opts)
+  map("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", map_opts)
+  map("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<cr>", map_opts)
+
+  -- These have a different style than above because I was fiddling
+  -- around and never converted them. Instead of converting them
+  -- now, I'm leaving them as they are for this article because this is
+  -- what I actually use, and hey, it works ¯\_(ツ)_/¯.
+  vim.cmd [[imap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']]
+  vim.cmd [[smap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']]
+
+  vim.cmd [[imap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>']]
+  vim.cmd [[smap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>']]
+  vim.cmd [[imap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>']]
+  vim.cmd [[smap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>']]
+
+  vim.cmd [[inoremap <silent><expr> <C-Space> compe#complete()]]
+  vim.cmd [[inoremap <silent><expr> <CR> compe#confirm('<CR>')]]
+  vim.cmd [[inoremap <silent><expr> <C-e> compe#close('<C-e>')]]
+  vim.cmd [[inoremap <silent><expr> <C-f> compe#scroll({ 'delta': +4 })]]
+  vim.cmd [[inoremap <silent><expr> <C-d> compe#scroll({ 'delta': -4 })]]
+
+  -- tell nvim-cmp about our desired capabilities
+  require("cmp_nvim_lsp").update_capabilities(capabilities)
+end
+
+local lspconfig = require("lspconfig")
+
+lspconfig.elixirls.setup({
+  cmd = {path_to_elixirls},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    elixirLS = {
+      dialyzerEnabled = true,
+      fetchDeps = false
+    }
+   }
+})
+
+--nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,60 @@
+local cmd = vim.cmd
+
+cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+    --- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    ---- Nord Color Scheme
+    use 'shaunsingh/nord.nvim'
+
+    ----- TreeSitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    ------ LSP
+    use {
+        'neovim/nvim-lspconfig',
+        'williamboman/nvim-lsp-installer',
+    }
+
+    -- autocomplete and snippets
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-vsnip")
+    use("hrsh7th/vim-vsnip")
+
+    use("onsails/lspkind-nvim")
+
+    use {
+      "folke/trouble.nvim",
+      requires = "kyazdani42/nvim-web-devicons",
+      config = function()
+        require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      }
+    end}
+
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use 'EdenEast/nightfox.nvim'
+
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
+    
+    use {
+     'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons',
+    }
+
+
+end)
