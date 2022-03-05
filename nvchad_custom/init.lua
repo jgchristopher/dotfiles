2f0797a62d921c14c5b71ebcc33ec35004ff5b60
John Christopher <jgchristopher@gmail.com>
2022-03-05 17:08:50 -0600

    trying to manage folding

--- nvchad_custom/init.lua
+++ nvchad_custom/init.lua
@@ -1,1 +1,4 @@
-require "custom.mappings"
+require("custom.mappings")
+
+vim.opt.foldexpr = "" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+vim.opt.foldmethod = "syntax" -- folding set to "expr" for treesitter based folding
--- nvchad_custom/null-ls.lua
+++ nvchad_custom/null-ls.lua
@@ -1,34 +1,34 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 local b = null_ls.builtins
 
 local sources = {
 
-   b.formatting.prettierd.with { filetypes = { "html", "markdown", "css", "typescript", "typescriptreact" } },
-   b.formatting.deno_fmt,
+	b.formatting.prettierd.with({ filetypes = { "heex", "html", "markdown", "css", "typescript", "typescriptreact" } }),
+	b.formatting.deno_fmt,
 
-   -- Lua
-   b.formatting.stylua,
-   b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+	-- Lua
+	b.formatting.stylua,
+	b.diagnostics.luacheck.with({ extra_args = { "--global vim" } }),
 
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 }
 
 local M = {}
 
 M.setup = function()
-   null_ls.setup {
-      debug = true,
-      sources = sources,
+	null_ls.setup({
+		debug = true,
+		sources = sources,
 
-      -- format on save
-      on_attach = function(client)
-         if client.resolved_capabilities.document_formatting then
-            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
-         end
-      end,
-   }
+		-- format on save
+		on_attach = function(client)
+			if client.resolved_capabilities.document_formatting then
+				vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+			end
+		end,
+	})
 end
 
 return M
