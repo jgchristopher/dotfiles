5bf42ae866fa3560d2dee8adb14d40b2c9c92ddc
John Christopher <jgchristopher@gmail.com>
2022-03-06 09:10:00 -0600

    add folding with default open

--- nvchad_custom/init.lua
+++ nvchad_custom/init.lua
@@ -1,4 +1,5 @@
 require("custom.mappings")
 
 vim.opt.foldexpr = "" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
-vim.opt.foldmethod = "syntax" -- folding set to "expr" for treesitter based folding
+vim.opt.foldmethod = "indent" -- folding set to "expr" for treesitter based folding
+vim.opt.foldlevel = 99 -- I don't want folding to be default closed
