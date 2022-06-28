15d213aed85d9f62009adf1e4f7ab8e64d936658
John Christopher <jgchristopher@gmail.com>
2022-06-28 08:44:32 -0500

    go back to local buffer lspkeymaps

--- nvim/lua/user/lspconfigs.lua
+++ nvim/lua/user/lspconfigs.lua
@@ -12,21 +12,19 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local on_attach = function(_, bufnr)
+	local function map(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+
 	local map_opts = { noremap = true, silent = true }
-	vim.keymap.set("n", "<space>r", vim.lsp.codelens.run, map_opts)
-	vim.keymap.set("n", "df", "<cmd>lua vim.lsp.buf.formatting_seq_sync()<cr>", map_opts)
-	vim.keymap.set("n", "gd", "<cmd>lua vim.diagnostic.open_float()<cr>", map_opts)
-	vim.keymap.set("n", "dt", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
-	vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", map_opts)
-	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<cr>", map_opts)
-	vim.keymap.set("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<cr>", map_opts)
-	-- keybinds for fzf-lsp.nvim: https://github.com/gfanto/fzf-lsp.nvim
-	-- you could also use telescope.nvim: https://github.com/nvim-telescope/telescope.nvim
-	-- there are also core vim.lsp functions that put the same data in the loclist
-	vim.keymap.set("n", "gr", ":References<cr>", map_opts)
-	vim.keymap.set("n", "g0", ":DocumentSymbols<cr>", map_opts)
-	vim.keymap.set("n", "gW", ":WorkspaceSymbols<cr>", map_opts)
-	vim.keymap.set("n", "<leader>d", ":Diagnostics<cr>", map_opts)
+
+	map("n", "df", "<cmd>lua vim.lsp.buf.formatting()<cr>", map_opts)
+	map("n", "gd", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>", map_opts)
+	map("n", "dt", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
+	map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", map_opts)
+	map("n", "gD", "<cmd>lua vim.lsp.buf.implementation()<cr>", map_opts)
+	map("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", map_opts)
+	map("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<cr>", map_opts)
 end
 
 local lspconfig = require("lspconfig")
