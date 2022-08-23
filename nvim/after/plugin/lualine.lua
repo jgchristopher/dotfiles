2ed182d29c51c401c5eaccd1ed118e83bef2f96c
John Christopher <jgchristopher@gmail.com>
2022-08-23 06:27:59 -0500

    adding a close dap ui keymap

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,7 +1,16 @@
 require("lualine").setup({
-	options = {
-		-- ... your lualine config
-		theme = "catppuccin",
+  options = {
+    -- ... your lualine config
+    theme = "catppuccin",
     globalstatus = true,
-	},
+  },
+  winbar = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { { 'filename', path = 1 } }
+  }
+
 })
