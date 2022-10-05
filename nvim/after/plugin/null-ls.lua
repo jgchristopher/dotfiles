d3e9d406641923c867fe649ed388df8144ecc244
John Christopher <jgchristopher@gmail.com>
2022-10-05 10:58:13 -0500

    fix format call and add gitsigns keybindings

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -6,7 +6,7 @@ null_ls.setup {
     if client.server_capabilities.documentFormattingProvider then
       vim.api.nvim_command [[augroup Format]]
       vim.api.nvim_command [[autocmd! * <buffer>]]
-      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
       vim.api.nvim_command [[augroup END]]
     end
   end,
