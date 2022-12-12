9cd85d75fde705ecaf7af648308bf88a8b5c7a58
John Christopher <jgchristopher@gmail.com>
2022-12-11 20:25:25 -0600

    fix weekly clipping/readings dataviews

--- obsidian/weekly_review_reviewed_dataview_queries.ts
+++ obsidian/weekly_review_reviewed_dataview_queries.ts
@@ -2,12 +2,16 @@ dv.header(2, "All Reviewed Clippings");
 dv.taskList(
   dv
     .pages('"daily_notes"')
-    .where((b) => {
-      b.file.ctime >=
-        moment("<% tp.date.now('YYYY-MM-DD')%>").subtract(6, "days") &&
-        b.file.cday <= moment("<% tp.date.now('YYYY-MM-DD')%>");
-    })
+    .where(
+      (b) =>
+        b.file.ctime >=
+          moment("<% tp.date.now('YYYY-MM-DD')%>").subtract(6, "days") &&
+        b.file.cday <= moment("<% tp.date.now('YYYY-MM-DD')%>")
+    )
     .file.tasks.where(
-      (t) => t.text.includes("#bookmark-clipper") && t.completed
+      (t) =>
+        (t.text.includes("#obsidian-clipper") ||
+          t.text.includes("#bookmark-clipper")) &&
+        t.completed
     )
 );
