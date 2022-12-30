d7cf489c8e25ea3352fd63730eb1723aaaf44384
John Christopher <jgchristopher@gmail.com>
2022-12-30 10:56:50 -0600

    remove null-ls on_attach. It seemed to be overriding other LSPs

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -2,14 +2,7 @@ local status, null_ls = pcall(require, 'null-ls')
 if (not status) then return end
 
 null_ls.setup {
-  on_attach = function(client, _)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.api.nvim_command [[augroup Format]]
-      vim.api.nvim_command [[autocmd! * <buffer>]]
-      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
-      vim.api.nvim_command [[augroup END]]
-    end
-  end,
+
   sources = {
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.formatting.prettierd
