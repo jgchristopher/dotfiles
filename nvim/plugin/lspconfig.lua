25b71293c1a8b87b4c47be7961ed2cffe49550af
John Christopher <jgchristopher@gmail.com>
2023-02-22 09:04:50 -0600

    adding eslint

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -30,20 +30,20 @@ local on_attach = function(client, _)
   -- local map_opts = { noremap = true, silent = true } -- --   local map_opts = { noremap = true, silent = true }
 
   wk.register({
-      l = {
-          name = "lsp",
-          f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format Code" },
-          i = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Go to implementation" },
-          r = { "<cmd>Lspsaga rename<cr>", "Rename Variable" },
-          d = { "<cmd>Lspsaga lsp_finder<CR>", "Lsp Saga Finder" },
-          p = { "<cmd>Lspsaga peek_definition<cr>", "Peek Definition" },
-          a = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
-          h = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover Doc" },
-      }
+    l = {
+      name = "lsp",
+      f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format Code" },
+      i = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Go to implementation" },
+      r = { "<cmd>Lspsaga rename<cr>", "Rename Variable" },
+      d = { "<cmd>Lspsaga lsp_finder<CR>", "Lsp Saga Finder" },
+      p = { "<cmd>Lspsaga peek_definition<cr>", "Peek Definition" },
+      a = { "<cmd>Lspsaga code_action<cr>", "Code Action" },
+      h = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover Doc" },
+    }
   }, { prefix = "<leader>" })
 
   wk.register({
-      e = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "Diagnostic Jump Previous" }
+    e = { "<cmd>Lspsaga diagnostic_jump_prev<CR>", "Diagnostic Jump Previous" }
   }, { prefix = "[" })
   --map("n", "df", "<cmd>lua vim.lsp.buf.format()<cr>", map_opts)
   --map("n", "gd", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>", map_opts)
@@ -56,52 +56,52 @@ local on_attach = function(client, _)
 end
 
 vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = false,
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
 })
 
 mason.setup {}
 
 mason_config.setup {
-    ensure_installed = { "tsserver", "tailwindcss", "lua_ls", }
+  ensure_installed = { "tsserver", "tailwindcss", "lua_ls", }
 }
 
 nvim_lsp.tsserver.setup {
-    on_attach = on_attach,
+  on_attach = on_attach,
 }
 
 nvim_lsp.lua_ls.setup {
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            }
-        }
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      }
     }
+  }
 }
 
 nvim_lsp.tailwindcss.setup {
-    on_attach = on_attach,
-    init_options = {
-        userLanguages = {
-            eelixir = "html", -- These are required if I won't to have tailwindcss suggestions in elixir heex templates
-            heex = "html"
-        }
-    },
-    tailwindCSS = {
-        emmetCompletions = true,
-        includeLanguages = {
-            astro = "html"
-        }
+  on_attach = on_attach,
+  init_options = {
+    userLanguages = {
+      eelixir = "html", -- These are required if I won't to have tailwindcss suggestions in elixir heex templates
+      heex = "html"
     }
+  },
+  tailwindCSS = {
+    emmetCompletions = true,
+    includeLanguages = {
+      astro = "html"
+    }
+  }
 }
 
 nvim_lsp.astro.setup {
@@ -111,6 +111,8 @@ nvim_lsp.astro.setup {
 nvim_lsp.svelte.setup {
 
 }
+
+nvim_lsp.eslint.setup {}
 
 --[[ nvim_lsp.tailwindcss.setup {
   cmd = { "tailwindcss-language-server", "--stdio" },
@@ -138,42 +140,42 @@ nvim_lsp.svelte.setup {
   }
 } ]]
 nvim_lsp.graphql.setup {
-    tailwindCSS = {
-        hovers = true,
-        suggestions = true,
-        codeActions = true
-    }
+  tailwindCSS = {
+    hovers = true,
+    suggestions = true,
+    codeActions = true
+  }
 }
 
 
 -- The Mason installed version of elixir ls seems to be old. Suggestions and stuff don't work with the same settings
 local path_to_elixirls = vim.fn.expand("~/gitprojects/elixir_projects/elixir-ls/release/language_server.sh")
 nvim_lsp.elixirls.setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    cmd = { path_to_elixirls }, -- uncomment the local path above and this if you want to use that instead of the mason installed version
-    -- settings = {
-    --   elixirLS = {
-    --     dialyzerEnabled        = true,
-    --     dialyzerFormat         = "dialyxir_long",
-    --     enableTestLenses       = false,
-    --     fetchDeps              = false,
-    --     mixEnv                 = "test",
-    --     projectDir             = "",
-    --     signatureAfterComplete = true,
-    --     suggestSpecs           = true,
-    --   }
-    -- }
+  capabilities = capabilities,
+  on_attach = on_attach,
+  cmd = { path_to_elixirls }, -- uncomment the local path above and this if you want to use that instead of the mason installed version
+  -- settings = {
+  --   elixirLS = {
+  --     dialyzerEnabled        = true,
+  --     dialyzerFormat         = "dialyxir_long",
+  --     enableTestLenses       = false,
+  --     fetchDeps              = false,
+  --     mixEnv                 = "test",
+  --     projectDir             = "",
+  --     signatureAfterComplete = true,
+  --     suggestSpecs           = true,
+  --   }
+  -- }
 }
 
 nvim_lsp.emmet_ls.setup({
-    capabilities = capabilities,
-    filetypes = { 'html', 'heex', 'eelixir', 'css' },
-    init_options = {
-        html = {
-            options = {
-                ["bem.enabled"] = true
-            }
-        }
+  capabilities = capabilities,
+  filetypes = { 'html', 'heex', 'eelixir', 'css' },
+  init_options = {
+    html = {
+      options = {
+        ["bem.enabled"] = true
+      }
     }
+  }
 })
