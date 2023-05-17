98a5793c73ac340eec37d7bb70f5be0b1753cd75
John Christopher <jgchristopher@gmail.com>
2023-05-17 15:14:33 -0500

    fish shell and starship prompt

---
+++ conf.d/abbr.fish
@@ -0,0 +1,7 @@
+abbr fishconfig "nvim ~/.zshrc"
+abbr localconfig "nvim ~/.local-path-vars"
+abbr lsplog "tail -f ~/.local/state/nvim/lsp.log"
+abbr cdga "cd ~/gitprojects/arbol/"
+abbr cdlp "cd ~/gitprojects/obsidian-plugins/obsidian-clipper-projects"
+abbr gitlog "git log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)%an%C(reset) - %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+abbr ls "lsd"
---
+++ functions/arbol_switch.fish
@@ -0,0 +1,3 @@
+function arbol_switch
+  set -gx AWS_PROFILE 'arbol-jc-dev'
+end
