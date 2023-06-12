5cab1b6749e348f9536f17e351a24c147755af9f
John Christopher <jgchristopher@gmail.com>
2023-06-12 14:21:45 -0500

    working function to choose a tmux session

--- fish/functions/tmux_list.fish
+++ fish/functions/tmux_list.fish
@@ -1,14 +1,19 @@
 function tmux_list 
-  set items default kickstart LazyVim NvChad
-  set config $(printf "%s\n" $items | fzf --prompt="Tmux Session >> " --height=~50% --layout=reverse --border --exit-0)
+  # Check if we are in a TMUX session already
+  if status is-interactive 
+    and not set -q TMUX
 
+    # If not then go ahead and get list of tmux sessions or new
 
-  echo "WTF!"
+    set items new (tmux list-sessions -F "#S") 
+    set config $(printf "%s\n" $items | fzf --prompt="Tmux Session >> " --height=~50% --layout=reverse --border --exit-0)
 
-  switch $config
-    case default
-      echo Default was Sent!
-    case '*'
-      echo $config
+    switch $config
+      case new 
+        read session
+        tmux new -s $session
+      case '*'
+        tmux attach-session -t $config
+    end
   end
 end
