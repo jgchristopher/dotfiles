cb5a8f737fff0ddc759d9f89422be713299ec4f6
John Christopher <jgchristopher@gmail.com>
2023-06-12 14:29:54 -0500

    add abbr for tmux_list function

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -6,3 +6,4 @@ abbr cdlp "cd ~/gitprojects/obsidian-plugins/obsidian-clipper-projects"
 abbr cdt "cd ~/gitprojects/dotfiles"
 abbr gitlog "git log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)%an%C(reset) - %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 abbr ls "lsd"
+abbr -a tms -f tmux_list
