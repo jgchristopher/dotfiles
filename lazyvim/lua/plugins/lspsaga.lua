1f57528f4530fa4512afa2c9f3a62181cb192047
John Christopher <jgchristopher@gmail.com>
2023-07-03 10:50:07 -0500

    adding barbecue and lspsaga to lazyvim. updating lsp keymaps

---
+++ lazyvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,39 @@
+return {
+  -- edit lsp keymaps
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- change a keymap
+      keys[#keys + 1] = { "<leader>cd", false }
+    end,
+  },
+  {
+    "glepnir/lspsaga.nvim",
+    keys = {
+      { "<leader>cd", "<cmd>Lspsaga lsp_finder<cr>", desc = "LspSaga Finder" },
+      { "<leader>cp", "<cmd>Lspsaga peek_definition<cr>", desc = "LspSaga Peek Definition" },
+    },
+    config = {
+      lightbulb = {
+        enable = false,
+      },
+      outline = {
+        win_position = "right",
+        win_with = "",
+        win_width = 30,
+        preview_width = 0.4,
+        show_detail = true,
+        auto_preview = true,
+        auto_refresh = true,
+        auto_close = true,
+        auto_resize = false,
+        custom_sort = nil,
+        keys = {
+          expand_or_jump = "o",
+          quit = "q",
+        },
+      },
+    },
+  },
+}
