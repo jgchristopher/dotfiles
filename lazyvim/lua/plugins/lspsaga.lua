0bc692fe60207517a56b55cdd311ddf3fbf59a2f
John Christopher <jgchristopher@gmail.com>
2023-07-04 20:13:27 -0500

    fix keymaps, add better escape

--- lazyvim/lua/plugins/lspsaga.lua
+++ lazyvim/lua/plugins/lspsaga.lua
@@ -6,6 +6,7 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- change a keymap
       keys[#keys + 1] = { "<leader>cd", false }
+      keys[#keys + 1] = { "<c-k>", mode = "i", false }
     end,
   },
   {
