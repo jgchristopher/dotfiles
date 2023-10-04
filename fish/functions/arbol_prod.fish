d72904dc9f7fc7f1d86a75d05d08ab5888e37cc2
John Christopher <jgchristopher@gmail.com>
2023-10-04 16:53:26 -0500

    add back colorizer

---
+++ fish/functions/arbol_prod.fish
@@ -0,0 +1,3 @@
+function arbol_prod
+    set -gx AWS_PROFILE arbol-prod #777308304002_AdministratorAccess
+end
---
+++ lazyvim/lua/plugins/colorizer.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      filetypes = {
+        "css",
+        "javascript",
+        html = { mode = "foreground" },
+      },
+      user_default_options = { mode = "background" },
+      tailwind = true,
+    },
+  },
+}
