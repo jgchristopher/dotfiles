843ca923e4db56ac747d78d66df0bf552b84997e
John Christopher <jgchristopher@gmail.com>
2023-10-17 14:14:37 -0500

    updating plugins and migrating dashboard

--- lazyvim/lua/plugins/dashboard.lua
+++ lazyvim/lua/plugins/dashboard.lua
@@ -1,5 +1,5 @@
 return {
-  "goolord/alpha-nvim",
+  "nvimdev/dashboard-nvim",
   opts = function(_, opts)
     local logo = [[
            ██╗ ██████╗    ██╗██████╗ ███████╗
@@ -9,15 +9,9 @@ return {
       ╚█████╔╝╚██████╗    ██║██████╔╝███████╗
        ╚════╝  ╚═════╝    ╚═╝╚═════╝ ╚══════╝
       ]]
-    opts.section.header.val = vim.split(logo, "\n", { trimempty = true })
-    opts.section.buttons.val = {
-      opts.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-      opts.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-      opts.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-      opts.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-      opts.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-      opts.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-      opts.button("q", " " .. " Quit", ":qa<CR>"),
-    }
+
+    logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+    opts.config.header = vim.split(logo, "\n", { trimempty = true })
   end,
 }
