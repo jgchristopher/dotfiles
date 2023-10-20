644491f77c831d4386dad28f53519e46e5723f36
John Christopher <jgchristopher@gmail.com>
2023-10-19 20:57:25 -0500

    tmux updates. use homebrew for asdf. update nvim plugins

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,3 +1,8 @@
+abbr dps "docker ps --format 'table {{.Names}}\t{{.Status}}'"
+
+
+
+
 abbr fishconfig "nvim ~/.zshrc"
 abbr localconfig "nvim ~/.local-path-vars"
 abbr lsplog "tail -f ~/.local/state/nvim/lsp.log"
@@ -5,5 +10,10 @@ abbr cdga "cd ~/gitprojects/arbol/"
 abbr cdlp "cd ~/gitprojects/obsidian-plugins/obsidian-clipper-projects"
 abbr cdt "cd ~/gitprojects/dotfiles"
 abbr gitlog "git log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)%an%C(reset) - %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-abbr ls "lsd"
 abbr -a tms -f tmux_list
+
+abbr l "lsd -latr"
+abbr ld lazydocker
+abbr lg lazygit
+abbr ll "lsd  --group-dirs first -Al"
+abbr lt "lsd  --group-dirs last -A --tree"
--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,8 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-source ~/.config/fish/conf.d/abbr.fish
-#source ~/.asdf/asdf.fish
-
+eval (/opt/homebrew/bin/brew shellenv)
+zoxide init fish | source
 starship init fish | source
 
-set -gx DOTFILES ~/gitprojects/dotfiles/
-fish_add_path ~/gitprojects/dotfiles/bin
-fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/sbin
+
 
 function __auto_add_local_node_bin --on-event fish_postexec
     set -l node_modules_path "$PWD/node_modules/.bin"
@@ -20,3 +12,9 @@ function __auto_add_local_node_bin --on-event fish_postexec
 end
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+
+set -Ux FZF_TMUX_OPTS "-p 55%,60%"
+
+fish_add_path $HOME/.config/bin
+fish_add_path $HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin
