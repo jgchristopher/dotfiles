5b14670fa5e1bd4585bd7c86aeec0e8df4ed9270
John Christopher <john@jdotc.xyz>
2023-10-31 21:07:34 -0500

    messing with elixir and lazy plugins

--- lazyvim/lua/plugins/elixir.lua
+++ lazyvim/lua/plugins/elixir.lua
@@ -5,22 +5,24 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       local elixir = require("elixir")
-      local elixirls = require("elixir.elixirls")
+      --local elixirls = require("elixir.elixirls")
 
       elixir.setup({
-        nextls = { enable = true },
+        nextls = {
+          enable = true,
+          init_options = {
+            mix_env = "dev",
+            mix_target = "host",
+            experimental = {
+              completions = {
+                enable = true, -- control if completions are enabled. defaults to false
+              },
+            },
+          },
+        },
         credo = {},
         elixirls = {
-          enable = true,
-          settings = elixirls.settings({
-            dialyzerEnabled = false,
-            enableTestLenses = false,
-          }),
-          on_attach = function(client, bufnr)
-            vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-          end,
+          enable = false,
         },
       })
     end,
--- lazyvim/lua/plugins/lspservers.lua
+++ lazyvim/lua/plugins/lspservers.lua
@@ -25,9 +25,7 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "elixir-ls",
-      })
+      vim.list_extend(opts.ensure_installed, {})
     end,
   },
   {
