5a514b334ef66c2fbad9daee963d6d4abc9bbb85
John Christopher <john@jdotc.xyz>
2023-11-09 13:35:15 -0600

    updating dashboard and elixir

--- lazyvim/lua/plugins/elixir.lua
+++ lazyvim/lua/plugins/elixir.lua
@@ -1,45 +1,22 @@
 return {
-  {
-    "elixir-tools/elixir-tools.nvim",
-    version = "*",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local elixir = require("elixir")
-      --local elixirls = require("elixir.elixirls")
 
-      elixir.setup({
-        nextls = {
-          enable = true,
-          init_options = {
-            mix_env = "dev",
-            mix_target = "host",
-            experimental = {
-              completions = {
-                enable = true, -- control if completions are enabled. defaults to false
-              },
-            },
+  "elixir-tools/elixir-tools.nvim",
+  opts = function(_, opts)
+    opts.nextls = {
+      enable = true,
+      init_options = {
+        mix_env = "dev",
+        mix_target = "host",
+        experimental = {
+          completions = {
+            enable = true, -- control if completions are enabled. defaults to false
           },
         },
-        credo = {},
-        elixirls = {
-          enable = false,
-        },
-      })
-    end,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      opts = {
-        ensure_installed = {
-          "elixir",
-          "heex",
-          "surface",
-          "erlang",
-          "glimmer",
-        },
       },
-    },
-  },
+    }
+    opts.credo = {}
+    opts.elixirls = {
+      enable = false,
+    }
+  end,
 }
