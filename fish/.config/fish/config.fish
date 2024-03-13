8304a1a5d7d86cd2f87f95b5c55dc15c933b4bde
John Christopher <john@jdotc.xyz>
2024-03-13 08:24:39 -0600

    cleaning up fish_variables and fix node_path bin to not modifiy fish_variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,17 @@ eval (/opt/homebrew/bin/brew shellenv)
 set -U fish_greeting # disable fish greeting
 set -Ux EDITOR nvim
 
-
+# updated my implementation from this https://github.com/oh-my-fish/plugin-node-binpath/blob/master/conf.d/plugin-node-binpath.fish
 function __auto_add_local_node_bin --on-event fish_postexec
     set -l node_modules_path "$PWD/node_modules/.bin"
     if test -e "$node_modules_path"
-        addpaths $node_modules_path
+        set -g __node_binpath "$node_modules_path"
+        set -x PATH $PATH $__node_binpath
+    else
+        set -q __node_binpath
+        and set -l index (contains -i -- $__node_binpath $PATH)
+        and set -e PATH[$index]
+        and set -e __node_binpath
     end
 end
 
