6e4eec71424ab47dbb7db49a6ea461a7a62b6b09
John Christopher <john@jdotc.xyz>
2024-06-11 09:45:45 -0500

    updating lazy and drop barbecue

--- nvim/.config/nvim/lua/plugins/barbecue.lua
+++ nvim/.config/nvim/lua/plugins/barbecue.lua
@@ -1,10 +1,10 @@
 return {
-  "utilyre/barbecue.nvim",
-  name = "barbecue",
-  version = "*",
-  dependencies = {
-    "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons", -- optional dependency
-  },
-  opts = {},
+  -- "utilyre/barbecue.nvim",
+  -- name = "barbecue",
+  -- version = "*",
+  -- dependencies = {
+  --   "SmiteshP/nvim-navic",
+  --   "nvim-tree/nvim-web-devicons", -- optional dependency
+  -- },
+  -- opts = {},
 }
