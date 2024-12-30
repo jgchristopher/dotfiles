1c0c4e80e314dc2c190584a71943137a9fa64a57
John Christopher <john@jdotc.xyz>
2024-12-30 17:34:52 -0600

    fix dashboard and lazy-lock

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,17 +1,31 @@
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function(_, opts)
-    local logo = [[
-           ██╗ ██████╗    ██╗██████╗ ███████╗
-           ██║██╔════╝    ██║██╔══██╗██╔════╝
-          ██║██║         ██║██║  ██║█████╗
-     ██   ██║██║         ██║██║  ██║██╔══╝
-      ╚█████╔╝╚██████╗    ██║██████╔╝███████╗
-       ╚════╝  ╚═════╝    ╚═╝╚═════╝ ╚══════╝
-      ]]
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
+            ██╗ ██████╗    ██╗██████╗ ███████╗
+            ██║██╔════╝    ██║██╔══██╗██╔════╝
+           ██║██║         ██║██║  ██║█████╗
+      ██   ██║██║         ██║██║  ██║██╔══╝
+       ╚█████╔╝╚██████╗    ██║██████╔╝███████╗
+        ╚════╝  ╚═════╝    ╚═╝╚═════╝ ╚══════╝
+       ]],
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
-
-    opts.config.header = vim.split(logo, "\n", { trimempty = true })
-  end,
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
+  },
 }
