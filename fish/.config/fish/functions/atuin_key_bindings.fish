b5e1132b86c4aa104ce19cb70112f0d1e66d0993
John Christopher <john@jdotc.xyz>
2025-10-04 10:51:59 -0500

    updat atuin fish bindings to not use -k

--- fish/.config/fish/functions/atuin_key_bindings.fish
+++ fish/.config/fish/functions/atuin_key_bindings.fish
@@ -2,12 +2,10 @@ status is-interactive; or exit 0
 
 function atuin_key_bindings
     bind \cr _atuin_search
-    bind -k up _atuin_bind_up
     bind \eOA _atuin_bind_up
     bind \e\[A _atuin_bind_up
     if bind -M insert >/dev/null 2>&1
         bind -M insert \cr _atuin_search
-        bind -M insert -k up _atuin_bind_up
         bind -M insert \eOA _atuin_bind_up
         bind -M insert \e\[A _atuin_bind_up
     end
